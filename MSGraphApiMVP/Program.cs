92c904b8847cb3e874942d27cd9120792f944638
morsee <42314964+ur-fault@users.noreply.github.com>
2023-11-15 19:35:23 +0100

    Initial commit
    Barely working data acquisition, not so much in DI
    
    Need appsettings.json in project dir with azure credentials

---
+++ MSGraphApiMVP/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+using Microsoft.Identity.Abstractions;
+using MSGraphApiMVP;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options => options.ServiceName = "PrometheusMVPProvider")
+    .ConfigureServices((_, services) => {
+        LoggerProviderOptions.RegisterProviderOptions<ConsoleLoggerOptions, ConsoleLoggerProvider>(services);
+
+        services.AddHostedService<WindowsBackgroundService>();
+
+        services.AddMicrosoftGraph(options => {
+            options.Scopes = new[] { "User.Read.All" };
+        });
+
+        //services.AddHttpClient();
+        //services.AddTokenAcquisition();
+
+        services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme);
+        services.AddInMemoryTokenCaches();
+    });
+
+var host = builder.Build();
+
+await host.RunAsync();
+
+//var factory = TokenAcquirerFactory.GetDefaultInstance();
+//var services = factory.Services;
+
+//services.AddMicrosoftGraph();
+
+//var serviceProvider = factory.Build();
+
+//var client = serviceProvider.GetRequiredService<GraphServiceClient>();
+//var users = await client.Users.GetAsync(r => r.Options.WithAppOnly());
+//users.Value.ForEach(u => Console.WriteLine(u.DisplayName));
---
+++ MSGraphApiMVP/WindowsBackgroundService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Microsoft.Identity.Abstractions;
+using Microsoft.Identity.Web;
+
+namespace MSGraphApiMVP;
+
+class WindowsBackgroundService : BackgroundService
+{
+    private readonly ILogger<WindowsBackgroundService> _logger;
+    //private readonly ITokenAcquisition _tokenAcquisition;
+    private readonly GraphServiceClient _graphServiceClient;
+
+    public WindowsBackgroundService(ILogger<WindowsBackgroundService> logger, GraphServiceClient graphServiceClient) {
+        _logger = logger;
+        //_tokenAcquisition = tokenAcquisition;
+        _graphServiceClient = graphServiceClient;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken) {
+        //var token = await _tokenAcquisition.GetAuthenticationResultForAppAsync("https://graph.microsoft.com/.default",
+        //    tokenAcquisitionOptions: new TokenAcquisitionOptions { CancellationToken = cancellationToken });
+        //await _graphServiceClient.Users.GetAsync(cancellationToken: cancellationToken);
+
+        //await TokenAcquirerFactory.GetDefaultInstance().GetTokenAcquirer()
+        //    .GetTokenForAppAsync("https://graph.microsoft.com/.default", cancellationToken: cancellationToken);
+
+        await _graphServiceClient.Users.GetAsync(o => o.Options.WithAppOnly(), cancellationToken);
+
+        //await _tokenAcquisition.GetAccessTokenForAppAsync("https://graph.microsoft.com/.default");
+    }
+}
